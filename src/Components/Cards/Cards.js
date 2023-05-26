de65c0ffa41894c276c9431d89760087d4bbf485
Nicolas <nicolasbournas@hotmail.fr>
2023-05-26 18:28:51 +0200

    mise en place des cards

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -1,18 +1,20 @@
 import React from "react";
-
-// Link Css
-import './Cards.css'
-
-import Data from '../../Datas/data.json'
+import Data from '../../Datas/data.json';
+import './Cards.css';
 
 export default function Cards() {
-    return (
-        <main className="main">
-            <div className="mainContainer">
-                <div className="mainCards">
-
-                </div>
-            </div>
-        </main>
-    )
+  return (
+    <main className="main">
+      <div className="mainContainer">
+        {Data.map((card) => (
+          <div className="mainCards" key={card.id}>
+            <a href="#">
+                <img className="mainCardsImg" src={card.cover} alt="Cover" />
+            </a>
+            <p className="mainCardsTitle">{card.title}</p>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,12 +4,14 @@ import React from "react";
 import Navbar from "../Layouts/Navbar/Navbar";
 import Footer from "../Layouts/Footer/Footer";
 import Background from "../Layouts/Background/Background";
+import Cards from "../Components/Cards/Cards";
 
 export default function Home() {
     return (
         <div>
             <Navbar />
             <Background />
+            <Cards />
             <Footer />
         </div>
     )
